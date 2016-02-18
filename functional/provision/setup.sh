a04850ff483f486aeb426000bbd00c616418d32d
Rene Engelhard <rene@rene-engelhard.de>
2016-02-18 17:33:24 +0100

    wgets download is too verbose on vagrant provison, use --no-verbose

--- functional/provision/setup.sh
+++ functional/provision/setup.sh
@@ -9,6 +9,6 @@ export PATH=${HOME}/go/bin:${PATH}
 if [ ! -f ${HOME}/go1.5.3.linux-amd64.tar.gz ]; then
   # Remove unfinished archive when you press Ctrl+C
   trap "rm -f ${HOME}/go1.5.3.linux-amd64.tar.gz" EXIT
-  wget --progress=dot:mega https://storage.googleapis.com/golang/go1.5.3.linux-amd64.tar.gz -P ${HOME}
+  wget --no-verbose https://storage.googleapis.com/golang/go1.5.3.linux-amd64.tar.gz -P ${HOME}
 fi
 tar -xf ${HOME}/go1.5.3.linux-amd64.tar.gz -C ${HOME}
