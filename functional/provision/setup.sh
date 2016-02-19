112c46d0241c2162af5b0e21991b4033c9b10c1c
kayrus <kay.diam@gmail.com>
2016-02-19 11:02:48 +0100

    Added start_etcd script

--- functional/provision/setup.sh
+++ functional/provision/setup.sh
@@ -8,7 +8,7 @@ export PATH=${HOME}/go/bin:${PATH}
 
 if [ ! -f ${HOME}/go1.5.3.linux-amd64.tar.gz ]; then
   # Remove unfinished archive when you press Ctrl+C
-  trap "rm -f ${HOME}/go1.5.3.linux-amd64.tar.gz" EXIT
+  trap "rm -f ${HOME}/go1.5.3.linux-amd64.tar.gz" INT TERM
   wget --no-verbose https://storage.googleapis.com/golang/go1.5.3.linux-amd64.tar.gz -P ${HOME}
 fi
 tar -xf ${HOME}/go1.5.3.linux-amd64.tar.gz -C ${HOME}
