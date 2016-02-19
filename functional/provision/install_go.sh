0d8375bbfb8310f474406ec46c7404f354fe1c68
Rene Engelhard <rene@rene-engelhard.de>
2016-02-19 13:58:23 +0100

    split out go1.5.3.linux-amd64.tar.gz as a var

--- functional/provision/install_go.sh
+++ functional/provision/install_go.sh
@@ -6,9 +6,11 @@ HOME=$(getent passwd "${USER_ID}" | cut -d: -f6)
 export GOROOT=${HOME}/go
 export PATH=${HOME}/go/bin:${PATH}
 
-if [ ! -f ${HOME}/go1.5.3.linux-amd64.tar.gz ]; then
+gover=1.5.3
+gotar=go${gover}.linux-amd64.tar.gz
+if [ ! -f ${HOME}/${gotar} ]; then
   # Remove unfinished archive when you press Ctrl+C
-  trap "rm -f ${HOME}/go1.5.3.linux-amd64.tar.gz" INT TERM
-  wget --no-verbose https://storage.googleapis.com/golang/go1.5.3.linux-amd64.tar.gz -P ${HOME}
+  trap "rm -f ${HOME}/${gotar}" INT TERM
+  wget --no-verbose https://storage.googleapis.com/golang/${gotar} -P ${HOME}
 fi
-tar -xf ${HOME}/go1.5.3.linux-amd64.tar.gz -C ${HOME}
+tar -xf ${HOME}/${gotar} -C ${HOME}
